874001cae3c629beddcbed8fd286b97986ab9c88
anuschka <sustic@gmail.com>
2018-09-21 23:40:39 +0200

    building solution to challenge 6

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function switchChannel(channelName){
+
+    console.log('Tuning into channel', channelName);
+    document.getElementById('message-name').innerHTML= channelName + " by " + "upgrading.never.helps";
+}
