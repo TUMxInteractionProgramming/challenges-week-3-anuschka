41b049777bd2b4b03a73fdce7c87611929d95001
anuschka <sustic@gmail.com>
2018-09-21 23:53:27 +0200

    Building solution challenge 6

--- js/script.js
+++ js/script.js
@@ -3,3 +3,4 @@ function switchChannel(channelName){
     console.log('Tuning into channel', channelName);
     document.getElementById('message-name').innerHTML= channelName + " by " + "upgrading.never.helps";
 }
+
